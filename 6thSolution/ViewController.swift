12396e8178ba79f2ea7bd926cdda37cf5b895de4
yaser <yaserbahrami@outlook.com>
2018-10-16 17:53:11 +0330

    change in button corner radius

--- 6thSolution/ViewController.swift
+++ 6thSolution/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    override func viewDidLayoutSubviews() {
+        okButton.layer.cornerRadius = CGFloat(okButton.frame.height / 3)
     }
 
 
